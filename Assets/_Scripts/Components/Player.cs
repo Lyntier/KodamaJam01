6d787d157e9f003c955f8e12037910e8c53ef206
Nick Versteeg <nick.versteeg@outlook.com>
2019-11-09 13:33:00 +0100

    Using Unity instead of GameMaker Studio

---
+++ Assets/_Scripts/Components/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class Player : MonoBehaviour
+    {
+
+
+        Collider2D _collider;
+        
+        Vector2 _velocity;
+        bool _jump;
+        bool _attack;
+
+        void Start()
+        {
+            _collider = GetComponent<Collider2D>();
+        }
+
+        void Update()
+        {
+            _velocity.x = Input.GetAxisRaw("Horizontal");
+            _jump = Input.GetKeyDown(KeyCode.Z);
+            _attack = Input.GetKeyDown(KeyCode.X);
+        }
+    }
+}
